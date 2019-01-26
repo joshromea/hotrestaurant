035e680237da6b93216481a9fb39cd9568e6b9fa
Josh Romea <josh.romea@gmail.com>
2019-01-26 13:20:32 -0700

    fixed routing

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,14 +2,14 @@ const path = require(`path`);
 
 module.exports = (app) => {
     app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "home.html"));
+        res.sendFile(path.join(__dirname, `../home.html`));
     })
 
     app.get("/reserve", (req, res) => {
-        res.sendFile(path.join(__dirname, "reserve.html"));
+        res.sendFile(path.join(__dirname, `../reserve.html`));
     })
 
     app.get("/tables", (req, res) => {
-        res.sendFile(path.join(__dirname, "tables.html"));
+        res.sendFile(path.join(__dirname, `../tables.html`));
     })
 }
