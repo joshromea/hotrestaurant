5b23def3311476ef579d2a3c19bec1c3e9cd4986
Josh Romea <josh.romea@gmail.com>
2019-01-26 11:36:43 -0700

    base js

---
+++ tableres.js
@@ -0,0 +1,10 @@
+class TableRes {
+    constructor(name, phone, email, uniqueID) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.uniqueID = uniqueID;
+    }
+}
+
+module.exports = TableRes;
